7c8f69801c653664c45dd5e03b95a45ff9f2d003
villageworks <villageworks@gmail.com>
2023-11-08 21:06:22 +0900

    initial commit

---
+++ src/main/java/com/example/demo/SpringKadai20Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKadai20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKadai20Application.class, args);
+	}
+
+}
